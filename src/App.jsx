c432aa1de150741e466d5a44dd03b009bf83b59b
eduardo <eduardo.nobrega1999@hotmail.com>
2023-06-16 11:00:50 -0300

    refactoring add game component

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,7 @@ function Square({ value, onSquareClick }) {
   )
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setXIsNext] = useState(true)
-
+function Board({ xIsNext, squares, onPLay }) {
   const winner = calculateWinner(squares)
   let status
   if (winner) {
@@ -32,8 +29,7 @@ export default function Board() {
     } else {
       nextSquares[index] = 'O'
     }
-    setSquares(nextSquares)
-    setXIsNext((prevState) => !prevState)
+    onPLay(nextSquares)
   }
 
   return (
@@ -55,6 +51,29 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  )
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true)
+  const [history, setHistory] = useState([Array(9).fill(null)])
+
+  const currentSquares = history[history.length - 1]
+
+  function handlePlay(nextSquares) {
+    setHistory((prevState) => [...prevState, nextSquares])
+    setXIsNext((prevState) => !prevState)
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPLay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
   )
 }
 
